bf005c19740cb0de352c4f073f931e75d14c954f
Karl Kadalipp <karl.kadalipp@finestmedia.ee>
2018-09-06 12:28:41 +0300

    UI-1 - Wind chill & data displaying improvements

--- src/App.js
+++ src/App.js
@@ -43,19 +43,19 @@ class App extends Component {
             <Column key={'precipitations'} field={'precipitations'} header={'Precipitations'} sortable="true"/>
             <Column key={'uvIndex'} field={'uvIndex'} header={'UV index'} sortable="true"/>
             <Column key={'wmoCode'} field={'wmoCode'} header={'WMO code'} sortable="true"/>
-            <Column key={'airPressure'} field={'airPressure'} header={'Air pressure'} sortable="true"/>
+            <Column key={'airPressure'} field={'airPressure'} header={'Pressure'} sortable="true"/>
             <Column key={'relativeHumidity'} field={'relativeHumidity'} header={'Relative humidity'} sortable="true"/>
-            <Column key={'airTemperature'} field={'airTemperature'} header={'Air temperature'} sortable="true"/>
-            <Column key={'waterLevel'} field={'waterLevel'} header={'Water level'} sortable="true"/>
-            <Column key={'waterLevelEh2000'} field={'waterLevelEh2000'} header={'Water level EH2000'} sortable="true"/>
-            <Column key={'waterTemperature'} field={'waterTemperature'} header={'Water temperature'} sortable="true"/>
-            <Column key={'windDirection'} field={'windDirection'} header={'Wind direction'} sortable="true"/>
-            <Column key={'windSpeed'} field={'windSpeed'} header={'Wind speed'} sortable="true"/>
-            <Column key={'windSpeedMax'} field={'windSpeedMax'} header={'Wind speed max'} sortable="true"/>
-            <Column key={'windChillC'} field={'windChillC'} header={'Wind chill C'} sortable="true"/>
-            <Column key={'windChillF'} field={'windChillF'} header={'Wind chill F'} sortable="true"/>
-            <Column key={'windChillMaxC'} field={'windChillMaxC'} header={'Wind chill Max C'} sortable="true"/>
-            <Column key={'windChillMaxF'} field={'windChillMaxF'} header={'Wind chill Max F'} sortable="true"/>
+            <Column key={'airTemperature'} field={'airTemperature'} header={'Temperature'} sortable="true"/>
+            <Column key={'waterLevel'} field={'waterLevel'} header={'Level'} sortable="true"/>
+            <Column key={'waterLevelEh2000'} field={'waterLevelEh2000'} header={'Level EH2000'} sortable="true"/>
+            <Column key={'waterTemperature'} field={'waterTemperature'} header={'Temp (C)'} sortable="true"/>
+            <Column key={'windDirection'} field={'windDirection'} header={'Direction'} sortable="true"/>
+            <Column key={'windSpeed'} field={'windSpeed'} header={'Speed (m/s)'} sortable="true"/>
+            <Column key={'windSpeedMax'} field={'windSpeedMax'} header={'Speed max'} sortable="true"/>
+            <Column key={'windChillC'} field={'windChillC'} header={'Chill (C)'} sortable="true"/>
+            <Column key={'windChillF'} field={'windChillF'} header={'Chill (F)'} sortable="true"/>
+            <Column key={'windChillMaxC'} field={'windChillMaxC'} header={'Chill Max (C)'} sortable="true"/>
+            <Column key={'windChillMaxF'} field={'windChillMaxF'} header={'Chill Max (F)'} sortable="true"/>
         </Row>;
 
         let headerGroup =
@@ -82,37 +82,35 @@ class App extends Component {
 
         let dataTable =
             <DataTable value={this.state.stations} resizableColumns={true} headerColumnGroup={headerGroup} footerColumnGroup={footerGroup}>
-                <Column key={'name'} field={'name'} header={'Name'} sortable="true"/>
-                <Column key={'longitude'} field={'longitude'} header={'Longitute'} sortable="true"/>
-                <Column key={'latitude'} field={'latitude'} header={'Latitude'} sortable="true"/>
-                <Column key={'phenomenon'} field={'phenomenon'} header={'Phenomenon'} sortable="true"/>
-                <Column key={'visibility'} field={'visibility'} header={'Visibility'} sortable="true"/>
-                <Column key={'precipitations'} field={'precipitations'} header={'Precipitations'} sortable="true"/>
-                <Column key={'uvIndex'} field={'uvIndex'} header={'UV index'} sortable="true"/>
-                <Column key={'wmoCode'} field={'wmoCode'} header={'WMO code'} sortable="true"/>
-                <Column key={'airPressure'} field={'airPressure'} header={'Air pressure'} sortable="true"/>
-                <Column key={'relativeHumidity'} field={'relativeHumidity'} header={'Relative humidity'} sortable="true"/>
-                <Column key={'airTemperature'} field={'airTemperature'} header={'Air temperature'} sortable="true"/>
-                <Column key={'waterLevel'} field={'waterLevel'} header={'Water level'} sortable="true"/>
-                <Column key={'waterLevelEh2000'} field={'waterLevelEh2000'} header={'Water level EH2000'} sortable="true"/>
-                <Column key={'waterTemperature'} field={'waterTemperature'} header={'Water temperature'} sortable="true"/>
-                <Column key={'windDirection'} field={'windDirection'} header={'Wind direction'} sortable="true"/>
-                <Column key={'windSpeed'} field={'windSpeed'} header={'Wind speed'} sortable="true"/>
-                <Column key={'windSpeedMax'} field={'windSpeedMax'} header={'Wind speed max'} sortable="true"/>
-                <Column key={'windChillC'} field={'windChillC'} header={'Wind chill C'} sortable="true"/>
-                <Column key={'windChillF'} field={'windChillF'} header={'Wind chill F'} sortable="true"/>
-                <Column key={'windChillMaxC'} field={'windChillMaxC'} header={'Wind chill Max C'} sortable="true"/>
-                <Column key={'windChillMaxF'} field={'windChillMaxF'} header={'Wind chill Max F'} sortable="true"/>
+                <Column key={'name'} field={'name'} header={'Name'} />
+                <Column key={'longitude'} field={'longitude'} />
+                <Column key={'latitude'} field={'latitude'} />
+                <Column key={'phenomenon'} field={'phenomenon'} />
+                <Column key={'visibility'} field={'visibility'} />
+                <Column key={'precipitations'} field={'precipitations'} />
+                <Column key={'uvIndex'} field={'uvIndex'} />
+                <Column key={'wmoCode'} field={'wmoCode'} />
+                <Column key={'airPressure'} field={'airPressure'} />
+                <Column key={'relativeHumidity'} field={'relativeHumidity'} />
+                <Column key={'airTemperature'} field={'airTemperature'} style={{fontWeight: "bold"}}/>
+                <Column key={'waterLevel'} field={'waterLevel'} />
+                <Column key={'waterLevelEh2000'} field={'waterLevelEh2000'} />
+                <Column key={'waterTemperature'} field={'waterTemperature'} />
+                <Column key={'windDirection'} field={'windDirection'} />
+                <Column key={'windSpeed'} field={'windSpeed'} style={{fontWeight: "bold"}}/>
+                <Column key={'windSpeedMax'} field={'windSpeedMax'} />
+                <Column key={'windChillC'} field={'windChillC'} style={{fontWeight: "bold"}}/>
+                <Column key={'windChillF'} field={'windChillF'} />
+                <Column key={'windChillMaxC'} field={'windChillMaxC'} />
+                <Column key={'windChillMaxF'} field={'windChillMaxF'} />
             </DataTable>;
 
         return (
             <div className="App">
                 <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo"/>
-                    <h1 className="App-title">Welcome</h1>
+                    <h1 className="App-title">Estonian weather stations</h1>
                 </header>
                 <div hidden={isLoading}>
-                    <h3>Estonian weather stations weather data:</h3>
                     {dataTable}
                 </div>
                 <Button label="Do something"/>
